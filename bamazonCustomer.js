db80ff8566864e7e83cbca5b33244b8291ef740e
John Falk <jrfalk@outlook.com>
2019-08-06 18:37:54 -0400

    Created connection to mySQL database, created function to display questions via inquirer regarding purchasing of items

---
+++ bamazonCustomer.js
@@ -0,0 +1,29 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Tomiko101387",
+    database: "bamazon_DB"
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    purchaseItem();
+});
+
+function purchaseItem() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Welcome to Bamazon",
+            choices: [
+                "What is the ID of the product that you would like to buy?",
+                "How many of the units would you like to buy?"
+            ]
+        })
+}
