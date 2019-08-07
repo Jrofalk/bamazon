ccc49494179278234ff366fb7676f7b5bb721c49
John Falk <jrfalk@outlook.com>
2019-08-07 14:57:14 -0400

    Added inquirer prompt after for loop in displayItems function that asks user for the ID of the item they would like to purchase as well as the amount of the product

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,33 +12,28 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    //Create function here that displays all bamazon items first. purchase Item will probably have to be outside of this function.
-    //reference topsongcode.js under top 5000 code
     displayItems();
-    //purchaseItem();
 });
 
 function displayItems() {
-    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
         if (err) throw err;
-        for(i=0; i<res.length; i++){
+        for (i = 0; i < res.length; i++) {
             console.log("-------------------------------------------------------------------------------");
             console.log("Product ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Price: $" + res[i].price);
             console.log("-------------------------------------------------------------------------------");
         }
+        inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Welcome to Bamazon",
+            choices: [
+                "What is the ID of the product that you would like to buy?",
+                "How many units would you like to buy?"
+            ]
+        })
         connection.end();
     })
 }
 
-//function purchaseItem() {
-    //inquirer
-        //.prompt({
-            //name: "action",
-            //type: "list",
-            //message: "Welcome to Bamazon",
-            //choices: [
-                //"What is the ID of the product that you would like to buy?",
-                //"How many units would you like to buy?"
-            //]
-       // })
-//}
