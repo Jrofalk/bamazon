7ad95f41b0edf569a2d100a87cb57c9145e0379d
John Falk <jrfalk@outlook.com>
2019-08-07 15:52:36 -0400

    Created empty choice array to store user response to ID number of product they would like to purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,7 +9,6 @@ var connection = mysql.createConnection({
     database: "bamazon_DB"
 });
 
-
 connection.connect(function (err) {
     if (err) throw err;
     displayItems();
@@ -24,16 +23,51 @@ function displayItems() {
             console.log("-------------------------------------------------------------------------------");
         }
         inquirer
-        .prompt({
-            name: "action",
-            type: "list",
-            message: "Welcome to Bamazon",
-            choices: [
-                "What is the ID of the product that you would like to buy?",
-                "How many units would you like to buy?"
-            ]
-        })
-        connection.end();
-    })
-}
+        .prompt([
+            {
+              name: "choice",
+              type: "input",
+              choices: function() {
+                var choiceArray = [];
+                for (var i = 0; i < res.length; i++) {
+                  choiceArray.push(res[i].item_id);
+                }
+                return choiceArray;
+              },
+              message: "What is the ID number of the product that you would like to buy?"
+            },
+            {
+              name: "amount",
+              type: "input",
+              message: "How many units of the product would you like to buy?"
+            }
+          ])
+          .then(function(answer) {
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+              if (results[i].item_name === answer.choice) {
+                chosenItem = results[i];
+              }
+            }
+    
+        //inquirer
+            //.prompt({
+               // name: "action",
+               // type: "list",
+               // message: "Welcome to Bamazon",
+               // choices: [
+                   // "What is the ID of the product that you would like to buy?",
+                    //"How many units would you like to buy?"
+               // ]
+            //})
 
+            //.then(function (answer) {
+               // console.log(answer);
+            //})
+
+
+       // connection.end();
+    })
+    }
+
+    )}
