175d00b50903bb7c6873891f7504554e51345b3e
John Falk <jrfalk@outlook.com>
2019-08-07 14:33:49 -0400

    Created for loop that iterates through MySQL data and displays tables information regarding product name, ID, and price

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,18 +12,32 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    purchaseItem();
+    //Create function here that displays all bamazon items first. purchase Item will probably have to be outside of this function.
+    //reference topsongcode.js under top 5000 code
+    displayItems();
+    //purchaseItem();
 });
 
-function purchaseItem() {
-    inquirer
-        .prompt({
-            name: "action",
-            type: "list",
-            message: "Welcome to Bamazon",
-            choices: [
-                "What is the ID of the product that you would like to buy?",
-                "How many units would you like to buy?"
-            ]
-        })
+function displayItems() {
+    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+        if (err) throw err;
+        for(i=0; i<res.length; i++){
+            console.log(res[i].item_id);
+        }
+        connection.end();
+   
+    })
 }
+
+//function purchaseItem() {
+    //inquirer
+        //.prompt({
+            //name: "action",
+            //type: "list",
+            //message: "Welcome to Bamazon",
+            //choices: [
+                //"What is the ID of the product that you would like to buy?",
+                //"How many units would you like to buy?"
+            //]
+       // })
+//}
