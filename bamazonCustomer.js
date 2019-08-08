e61c5bec5809efdb94c43c0d97dfe8e8168c5903
John Falk <jrfalk@outlook.com>
2019-08-07 21:26:18 -0400

    Removed array that stores user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,51 +23,45 @@ function displayItems() {
             console.log("-------------------------------------------------------------------------------");
         }
         inquirer
-        .prompt([
-            {
-              name: "choice",
-              type: "input",
-              choices: function() {
-                var choiceArray = [];
-                for (var i = 0; i < res.length; i++) {
-                  choiceArray.push(res[i].item_id);
-                }
-                return choiceArray;
-              },
-              message: "What is the ID number of the product that you would like to buy?"
-            },
-            {
-              name: "amount",
-              type: "input",
-              message: "How many units of the product would you like to buy?"
-            }
-          ])
-          .then(function(answer) {
-            var chosenItem;
-            for (var i = 0; i < results.length; i++) {
-              if (results[i].item_name === answer.choice) {
-                chosenItem = results[i];
-              }
-            }
-    
-        //inquirer
-            //.prompt({
-               // name: "action",
-               // type: "list",
-               // message: "Welcome to Bamazon",
-               // choices: [
-                   // "What is the ID of the product that you would like to buy?",
-                    //"How many units would you like to buy?"
-               // ]
-            //})
+            .prompt([
+                {
+                    name: "choice",
+                    type: "input",
+                    message: "What is the ID number of the product that you would like to buy?"
+                },
+                //{
+                // name: "amount",
+                //type: "input",
+                //message: "How many units of the product would you like to buy?"
+                //}
+            ])
+            .then(function (answer) {
+                var query = "SELECT item_id FROM bamazon_DB WHERE ?";
+                connection.query(query, { item_id: answer }, function (err, res) {
+                    for (var i = 0; i < res.length; i++) {
+                        console.log(res[i].item_id);
+                    }
+                })
 
-            //.then(function (answer) {
-               // console.log(answer);
-            //})
+                //inquirer
+                //.prompt({
+                // name: "action",
+                // type: "list",
+                // message: "Welcome to Bamazon",
+                // choices: [
+                // "What is the ID of the product that you would like to buy?",
+                //"How many units would you like to buy?"
+                // ]
+                //})
+
+                //.then(function (answer) {
+                // console.log(answer);
+                //})
 
 
-       // connection.end();
-    })
+                connection.end();
+            })
     }
 
-    )}
+    )
+}
